3d48f86dcfb5058dbde392b8b880bb454828dc2f
Ran Bao <TypeiD@ranbao-mbp.lan>
2013-04-27 16:06:13 +1200

    Initial Commit

---
+++ OpenGL_Timer_CPP/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenGL_Timer_CPP
+//
+//  Created by Ran Bao on 4/27/13.
+//  Copyright (c) 2013 Ran Bao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
